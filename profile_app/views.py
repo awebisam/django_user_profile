a13393d3c617242bc83401787dc79159bd5ac044
Aashish Bhandari <admin@aashish.com.np>
2020-08-12 14:24:06 +0545

    added intro api(more to be added)

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    '''Test API View'''
+
+    def get(self, request, format=None):
+        """Returns A list of APIView features"""
+        an_apiview = [
+            'Uses Http methods as f(get, put, post, patch and delete)',
+            'Similar to a Django CBV',
+            'Gives more control',
+            'Mapped Manually',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
